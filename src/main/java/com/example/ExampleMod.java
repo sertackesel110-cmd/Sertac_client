4934b548ee6c3e6bed99fcb2a8ea123c201a980b
sertackesel110-cmd <sertackesel110@gmail.com>
2026-03-25 15:26:16 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,21 @@
-package com.example;
+package net.fabricmc.example;
 
-import net.fabricmc.api.ModInitializer;
+public abstract class Module {
+    private String name;
+    private boolean enabled = false;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+    public Module(String name) {
+        this.name = name;
+    }
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    public String getName() { return name; }
+    public boolean isEnabled() { return enabled; }
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public void toggle() {
+        this.enabled = !this.enabled;
+        if (enabled) onEnable(); else onDisable();
+    }
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    public abstract void onEnable();
+    public abstract void onDisable();
 }
