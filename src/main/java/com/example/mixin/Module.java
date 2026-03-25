cc0b09c113aa564b99af63d7f67cefc0dfd71422
sertackesel110-cmd <sertackesel110@gmail.com>
2026-03-25 15:35:03 +0300

    Create Module.java

---
+++ src/main/java/com/example/mixin/Module.java
@@ -0,0 +1,15 @@
+package net.fabricmc.example;
+
+public abstract class Module {
+    private String name;
+    private boolean enabled = false;
+    public Module(String name) { this.name = name; }
+    public String getName() { return name; }
+    public boolean isEnabled() { return enabled; }
+    public void toggle() {
+        this.enabled = !this.enabled;
+        if (enabled) onEnable(); else onDisable();
+    }
+    public abstract void onEnable();
+    public abstract void onDisable();
+}
